06a3b726cd1fbd76682c122992a4865b9402470a
klaus-abram <sf.1951@mail.ru>
2021-10-04 02:44:31 +0300

    --links

--- api/external/owmadapter/adapter.go
+++ api/external/owmadapter/adapter.go
@@ -4,6 +4,7 @@ import "github.com/klaus-abram/suncold-restful-app/models"
 
 var owmSet = struct{ Metric, Lang string }{Metric: "C", Lang: "RU"}
 
+//links
 type OwmInterface interface {
 	GetOwmWeatherData(location string) (*models.WeatherResponse, error)
 	GetForecastInfo(location string) ([]models.WeatherResponse, error)
